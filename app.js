023cb2c35f8fce0056ed5f5cded2e54961f84cde
Miheer Vilas Tamkhane <sarangtamkhane10@gmail.com>
2021-07-30 09:44:01 +0530

    add more code

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const small1 = document.getElementById("small1");
 const small2 = document.getElementById("small2");
 const small3 = document.getElementById("small3");
 
-//function
+//function for calculating stocks
 function stockHandler() {
   const buyPrice = inputOne.value;
   const stockQty = inputTwo.value;
@@ -21,21 +21,21 @@ function stockHandler() {
   // console.log(totalGainOrLoss.toFixed(2));
   const percentageGainOrLoss = ((currentPrice - buyPrice) / buyPrice) * 100;
   // console.log(percentageGainOrLoss.toFixed(2) + "%");
-
+  //message + condition to check users loss is more than or == 50%
   let message = "";
-
   if (percentageGainOrLoss < 0 || percentageGainOrLoss <= -50) {
     document.body.style.backgroundColor = "#FF5D6C";
-    message = "Ohh! You lose money.";
-    output.classList.add("danger");
+    message = "Ohh! You lose money. 😞";
+    output.classList.add("danger"); //when lose money
     output.classList.remove("success");
   } else if (percentageGainOrLoss >= 0) {
     document.body.style.backgroundColor = "#acffad";
-    message = "Awwsome! You gain money.";
-    output.classList.add("success");
+    message = "Awwsome! You gain money. 😃";
+    output.classList.add("success"); // When gain money
   }
   if (buyPrice > 0 && stockQty > 0 && currentPrice > 0) {
-    output.innerHTML = `
+    //if all fileds are filled and more than 0 then show output
+    output.innerHTML = `<hr class="hr-tag" />
     <h2>${message}</h2>
   <ul class="output-ul">
   <li>Total Investment is : ${investment.toFixed(2)}</li>
@@ -50,19 +50,23 @@ function stockHandler() {
 `;
   }
 
+  //condition for showing error in <small> when fields are empty or = to 0
   if (buyPrice === "" || buyPrice == 0) {
     //
     small1.innerText = "Please enter buy price";
     small2.innerText = "";
+    document.body.style.backgroundColor = "#e6e6e6";
   } else if (stockQty === "" || stockQty == 0) {
     //
     small2.innerText = "Must enter stock quantity";
     small1.innerText = "";
+    document.body.style.backgroundColor = "#e6e6e6";
   } else if (currentPrice === "" || currentPrice == 0) {
     //
     small3.innerText = "Must enter current price";
     small1.innerText = "";
     small2.innerText = "";
+    document.body.style.backgroundColor = "#e6e6e6";
   } else {
     small3.innerText = "";
     small2.innerText = "";
